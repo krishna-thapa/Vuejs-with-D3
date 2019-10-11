280eeb40adb3bad9a6e124ead8d3a4e251ededed
krishna thapa <krishna.nature91@gmail.com>
2019-10-11 17:24:29 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" class="logo" src="./assets/logo.png">
+    <img alt="Vue logo" class="middle-logo" src="./assets/handshake.png">
+    <img alt="Vue logo" class="logo" src="./assets/d3.png">
+    <CircleChart msg="Combining D3.js with Vue.js"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CircleChart from './components/CircleChart.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CircleChart
   }
 }
 </script>
@@ -24,5 +26,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  width: 100px;
+}
+.middle-logo {
+  height: 50px;
 }
 </style>
